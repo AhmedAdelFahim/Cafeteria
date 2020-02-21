fe6a87b3c42c532c7ff0d3c8a2042ceb75c7b034
Kareem Morsy <kareemmorsy30@gmail.com>
2020-02-21 21:25:58 +0200

    set, remive and save orders to database

---
+++ Controllers/ordersController.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once ("Models/Order.php");
+
+class ordersController {
+    public function adminSetOrders($id)
+    {
+        $cookie_name = "orders";
+        
+        if(!isset($_COOKIE[$cookie_name])) {
+            $cookie_value = array($id);
+        }else{
+            $cookie_value = explode(", " , $_COOKIE[$cookie_name]);
+
+            if(!in_array ( $id, $cookie_value )){
+                array_push($cookie_value, $id);
+            }
+        }
+
+        setcookie($cookie_name, implode(", " ,$cookie_value), time() + (86400), "/");
+    }
+
+    public function adminRemoveOrder($id)
+    {
+        $cookie_name = "orders";
+        
+        if(isset($_COOKIE[$cookie_name])) {
+            $cookie_value = explode(", " , $_COOKIE[$cookie_name]);
+
+            foreach($cookie_value as $k => $prod_id){
+                if($prod_id == $id){
+                    break;
+                }
+            }
+
+            unset($cookie_value[$k]);
+            $cookie_value = array_values($cookie_value);
+
+            setcookie($cookie_name, implode(", " ,$cookie_value), time() + (86400), "/");
+        }
+    }
+
+    public function adminSaveOrder($products_id, $user_id, $notes, $prices)
+    {
+        $order = new Order;
+        
+        foreach($products_id as $k => $product_id){
+            $order->insert(NULL, $product_id , $user_id , $prices[$k] , $notes);
+        }
+        
+    }
+}
--- createOrder.php
+++ createOrder.php
@@ -11,11 +11,27 @@
     <?php
         require_once("Models/Product.php"); 
         require_once("Models/User.php");
+        require_once("Controllers/ordersController.php");
         $product = new Product;
         $user = new User;
 
         $products = $product->All();
         $users = $user->All();
+        $price = 0;
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['status']))
+        {
+            $order = new ordersController;
+            if($_GET['status'] == 'set'){
+                $order->adminSetOrders($_GET['id']);
+            }elseif($_GET['status'] == 'remove'){
+                $order->adminRemoveOrder($_GET['id']);
+            }
+            header("Location: http://127.0.0.1/Cafeteria/createOrder.php");
+        }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $order = new ordersController;
+            $order->adminSaveOrder($_POST['products'], $_POST['user'], $_POST['notes'], $_POST['price'] );
+        }
     ?>
     <div id="container">
         <div class="title">
@@ -34,24 +50,48 @@
         </div>
         <div id="bodyContainer">
             <div id="formContainer">
-                <form id="form" class="addproduct" method="POST" action="addproduct.php">
+                <form id="form" class="addproduct" method="POST" action="">
                     <table class="notes">
+                        <?php
+                        if(isset($_COOKIE['orders'])){
+                            $orders = explode(", " ,$_COOKIE['orders']);
+ 
+                            foreach($products as $product){
+
+                                if(in_array($product->id,$orders)){
+                                    $price = $price + $product->price;
+                        ?>
+                                    <tr id="selectedProds">
+                                        <td><?php echo $product->name; ?></td>
+                                        <td><input type="number" name="quantity[]" id="quantity" value="1"><input type="hidden" name="products[]" value="<? echo $product->id ?>"><input type="hidden" name="price[]" value="<? echo $product->price; ?>"></td>
+                                        <td><h4>EGP <? echo $product->price ?></h4></td>
+                                        <td><button type="button" onclick="removeOrder(<? echo $product->id; ?>)">X</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td><hr></td>
+                                    </tr>
+                        <?php
+                                }
+                            }
+                        }
+                        ?>
                         <tr>
 
                             <td> Notes </td>
 
-                            <td> <textarea></textarea> </td>
+                            <td> <textarea name="notes"></textarea> </td>
                         </tr>
 
                         <tr id="room">
                             <td> Room </td>
 
                             <td> 
-                                <select id="roomValues">
-                                    <option value="volvo">Volvo</option>
-                                    <option value="saab">Saab</option>
-                                    <option value="mercedes">Mercedes</option>
-                                    <option value="audi">Audi</option>
+                                <select name="room" id="roomValues">
+                                    <option value="100">100</option>
+                                    <option value="200">200</option>
+                                    <option value="300">300</option>
+                                    <option value="400">400</option>
                                 </select>
                             </td>
 
@@ -67,7 +107,7 @@
 
                         <tr id="productpicture">
                             <td></td>
-                            <td id="price"><h4>EGP 360</h4></td>
+                            <td id="price"><h4>EGP <? if(isset($price)){ echo $price; }else{ echo 0; } ?></h4></td>
 
                         </tr>
 
@@ -78,7 +118,7 @@
                             </td>
                         </tr>
                     </table>
-                </form>
+                
             </div>
             <div id="allProductsContainer">
                 <div id="search">
@@ -87,12 +127,12 @@
                 </div>
                 <div id="selectContainer">
                     <h4>Add to user</h4>
-                    <form id="form" class="selectUser" method="POST" action="addproduct.php">
+                    
                         <table class="notes">
                             <tr>
 
                                 <td> 
-                                    <select id="allUsers">
+                                    <select name="user" id="allUsers">
                                         <?php
                                             foreach($users as $user) {
                                         ?>
@@ -111,7 +151,7 @@
                             foreach($products as $product){
                         ?>
                         <div class="product">
-                            <img src="Assets/coffee.png"/>
+                            <a href="createOrder.php?status=set&id=<?php echo $product->id; ?>"><img src="Assets/coffee.png"/></a>
                             <h4><?php echo $product->name; ?></h4>
                         </div>
                         <?php
@@ -130,6 +170,11 @@
         }
     </style>
     <script src="https://kit.fontawesome.com/20351afc5f.js" crossorigin="anonymous"></script>
+    <script>
+        function removeOrder(id) {
+            window.location.href = `createOrder.php/?status=remove&id=${id}`;
+        }
+    </script>
 </body>
 
 </html>
