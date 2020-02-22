46c2941ce07d245435bfb46b971e14b32d4ddab5
Eman <emanmagdy961@gmail.com>
2020-02-22 17:06:23 +0200

    update cafeteria database

--- cafeteria.sql
+++ cafeteria.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2020 at 09:44 PM
+-- Generation Time: Feb 22, 2020 at 03:54 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
-
-CREATE DATABASE IF NOT EXISTS `cafeteria`/* !40100 DEFAULT CHARACTER SET latin1 */;
-USE `cafeteria`;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -20,8 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
-
 
 --
 -- Database: `cafeteria`
@@ -36,8 +31,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(255) NOT NULL,
   `name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -48,12 +43,13 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `orders` (
   `id` int(255) NOT NULL,
-  `date` date NOT NULL,
   `status` varchar(255) COLLATE utf8_bin NOT NULL,
   `product_id` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
-  `total_price` int(255) NOT NULL,
-  `notes` varchar(255) COLLATE utf8_bin NOT NULL
+  `total price` int(255) NOT NULL,
+  `notes` varchar(255) COLLATE utf8_bin NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -68,8 +64,8 @@ CREATE TABLE `products` (
   `price` int(255) NOT NULL,
   `picture` varchar(255) COLLATE utf8_bin NOT NULL,
   `category_id` int(255) NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -87,8 +83,21 @@ CREATE TABLE `users` (
   `ext` int(255) NOT NULL,
   `picture` varchar(255) COLLATE utf8_bin NOT NULL,
   `privilege` varchar(255) COLLATE utf8_bin NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_orders`
+--
+
+CREATE TABLE `users_orders` (
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `product_id` int(255) NOT NULL,
+  `order_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -123,6 +132,15 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -151,6 +169,12 @@ ALTER TABLE `users`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -166,6 +190,14 @@ ALTER TABLE `orders`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD CONSTRAINT `users_orders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `users_orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `users_orders_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
