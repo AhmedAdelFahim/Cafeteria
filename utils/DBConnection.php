2aba35c1bc2a48761824dc59deabf61936e3c5c3
ebtsam <ebtsamali620@gmail.com>
2020-02-22 16:11:01 +0200

    create login page and css page

--- utils/DBConnection.php
+++ utils/DBConnection.php
@@ -1,32 +1,37 @@
 <?php
+
 namespace database_connection;
+
 use PDO;
-    class DBConnection {
-        private static $instance=null;
+
+class DBConnection
+{
+    private static $instance = null;
 //        private static $servername = "localhost";
-        private static $dsn = 'mysql:host=localhost;dbname=cafeteria';
-        private static $username = "root";
-        private static $password = "";
-        private function __construct()
-        {
-            try {
-                self::$instance = new PDO(self::$dsn, self::$username, self::$password);
-            } catch (PDOException $e) {
-                echo "MySql Connection Error: " . $e->getMessage();
-            }
+    private static $dsn = 'mysql:host=localhost;dbname=cafeteria';
+    private static $username = 'root';
+    private static $password = '';
+
+    private function __construct()
+    {
+        try {
+            self::$instance = new PDO(self::$dsn, self::$username, self::$password);
+        } catch (PDOException $e) {
+            echo 'MySql Connection Error: '.$e->getMessage();
         }
-
-        public static function getInstance() {
-
-            if (!self::$instance) {
-                new DBConnection();
-            }
-
-            return self::$instance;
-        }
-
-        public static function disconnect(){
-            self::$instance=null;
-        }
-
     }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            new DBConnection();
+        }
+
+        return self::$instance;
+    }
+
+    public static function disconnect()
+    {
+        self::$instance = null;
+    }
+}
