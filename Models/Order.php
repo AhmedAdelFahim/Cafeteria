28cb364982ebf3a2563c7f1ee187ef65d7348e12
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-02-23 00:02:15 +0200

    order page added

--- Models/Order.php
+++ Models/Order.php
@@ -24,6 +24,37 @@ class Order {
         return $rows;
     }
 
+    function getWaitedOrders(){
+        $result = [];
+        $stmt=self::$db->prepare("SELECT id,created_at as order_date,total_price FROM `orders` WHERE `status` = ?");
+        $stmt->execute([0]);
+        $orders = $stmt->fetchAll(PDO::FETCH_OBJ);
+//        var_dump($row);
+        foreach($orders as $order)
+        {
+            $resultObj = new stdClass();
+            $resultObj->order_date = $order->order_date;
+            $resultObj->total_price = $order->total_price;
+            $stmt2=self::$db->prepare("SELECT DISTINCT users.name as `name` ,users.roomNo as room , users.ext FROM users_orders,users WHERE users_orders.user_id = users.id AND users_orders.order_id = ?");
+            $stmt2->execute([$order->id]);
+            $userData = $stmt2->fetchAll(PDO::FETCH_OBJ);
+//            var_dump($userData);
+            $resultObj->name = $userData[0]->name;
+            $resultObj->room = $userData[0]->room;
+            $resultObj->ext = $userData[0]->ext;
+            $stmt3=self::$db->prepare("SELECT users_orders.total_price as total_price , users_orders.amount as amount, products.name as `name` ,products.picture as picture FROM users_orders,products WHERE products.id = users_orders.product_id and users_orders.order_id = ?");
+            $stmt3->execute([$order->id]);
+            $productsData = $stmt3->fetchAll(PDO::FETCH_OBJ);
+//            var_dump($productsData);
+            $resultObj->products = $productsData;
+//            echo "\n";
+            array_push($result,$resultObj);
+
+        }
+//        var_dump($result);
+        return $result;
+    }
+
     function insert($status = 0 , $product_id , $user_id , $total_price , $notes)
     {
         $query = "INSERT INTO orders (`status`, `product_id`, `user_id`, `price`, `notes`, `created_at`, `updated_at`) VALUES ( ? , ? , ? , ? , ? , ? , ?);";
