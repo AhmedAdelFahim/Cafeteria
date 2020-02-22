9fccce0fd098e4e87c74d6bfea30bccc7c469b12
Nohahossam10 <nohahossam38@gmail.com>
2020-02-22 21:23:04 +0200

    modify of add user page

--- addinguser.js
+++ addinguser.js
@@ -1,10 +1,18 @@
+console.log("AAA");
 let saveBtn = document.getElementById("Save") ;
+
 saveBtn.addEventListener('click', function(event){
-event.preventDefault() ;
-validate() ;
-}) ;
+    let f = validate();
+    console.log(f);
+    if(f=== true)
+    {
+        event.preventDefault() ;
+    }
+});
+
 function validate()
 {
+    
 let userName = document.getElementById("name").value ;
 let Email = document.getElementById("email").value ;
 let Password = document.getElementById("password").value ;
@@ -12,14 +20,20 @@ let confirmPass = document.getElementById("cPassword").value ;
 let roomNumber = document.getElementById("room_No").value ;
 let EXT = document.getElementById("Ext").value ;
 let profilePic = document.getElementById("Profile").value ;
-    if(userName === "")
+    let flag=false;
+    console.log(userName.trim().length);
+    if(userName.trim().length === 0)
     {
         document.getElementById("errorName").innerHTML="enter your name" ;
+        console.log(flag,1);
+        flag = true ;
     }
 
-    if(Email == "")
+    if(Email.trim().length === 0)
     {
         document.getElementById("errorEmail").innerHTML="you must enter email" ;
+        console.log(flag,2);
+        flag = true ;
     }
     else
     {
@@ -27,11 +41,15 @@ let profilePic = document.getElementById("Profile").value ;
         if (!checkEmail)
         {
             document.getElementById("errorEmail").innerHTML="invalid email" ;
+            console.log(flag,3);
+            flag = true ;
         }
     }
-    if(Password === "")
+    if(Password.trim().length === 0)
     {
         document.getElementById("errorPass").innerHTML="you must enter password" ;
+        console.log(flag,4);
+        flag = true ;
     }
     else
     {
@@ -39,41 +57,58 @@ let profilePic = document.getElementById("Profile").value ;
         if( Password.length < 8 )
         {
             document.getElementById("errorPass").innerHTML="your password must greater than or equle to 8 char." ;
+            console.log(flag,5);
+            flag = true ;
         }
         let checkUnderScore = /[\'^£$%&*()}{@#~?><>,|=+¬-]/.test(Password) ;
         if(checkUnderScore)
         {
             document.getElementById("errorPass").innerHTML="your password contains only underScore" ;
+            console.log(flag,6);
+            flag = true ;
         }
         let checkUpper = /[A-Z]/.test(Password)
         if(checkUpper)
         {
             document.getElementById("errorPass").innerHTML="your passwordcan't contain upper case" ;
+            console.log(flag,7);
+            flag = true ;
         }
     }
-    if(confirmPass === "")
+    if(confirmPass.trim().length === 0)
     {
         document.getElementById("errorCpass").innerHTML="you must enter confirm password" ;
+        console.log(flag,8);
+        flag = true ;
     }
     else
     {
         if(confirmPass !== Password)
         {
             document.getElementById("errorCpass").innerHTML=" passwords aren't match!" ;
+            console.log(flag,9);
+            flag = true ;
         }
     }
-    if(roomNumber === "")
+    if(roomNumber.trim().length === 0)
     {
         document.getElementById("errorRoom").innerHTML="you must enter your room number" ;
+        console.log(flag,10);
+        flag = true ;
     }
     if(EXT === "")
     {
         document.getElementById("errorExt").innerHTML="you must enter ext" ;
+        console.log(flag,11);
+        flag = true ;
     }
-    if(profilePic === "")
+    if(profilePic.trim().length === 0)
     {
         document.getElementById("errorPic").innerHTML="you must enter your picture " ;
+        console.log(flag,12);
+        flag = true ;
 
     }
+    return flag ;
 }
  
