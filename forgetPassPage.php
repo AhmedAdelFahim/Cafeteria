648c4b7f1a5af14d8d7b91aeed473e6bfcee2f2d
ebtsam <ebtsamali620@gmail.com>
2020-02-22 16:28:02 +0200

    create forget password page and css style

---
+++ forgetPassPage.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>Forget-Password</title>
+        <link href="public/css/forgetPassPage.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="headerName">Cafeteria</h1>
+            <form class="formBox" name="loginForm" action="updatePassword.php" method="post" enctype="">
+                <label class="passLabel">New Password</label>
+                <input type="password" value="" placeholder="Enter new Password" name="passValue" class="passBox" id="passBox">
+                <br><br><br><br>
+
+                <label class="confirmPassLabel">Confirm new Password</label>
+                <input type="password" value="" placeholder="Confirm new Password" name="confirmPassValue" class="confirmPassBox" id="confirmPassBox">
+                <br>
+                <label id="error" class="error"></label>
+                <br><br>
+
+                <input type="submit" value="Submit" name="submitButton" class="submitBox" id="submitBox">
+            </form>
+
+        </div>
+
+        <script>
+            function checkPass (){
+                passValue = document.getElementById("passBox").value;
+                confirmPassValue = document.getElementById("confirmPassBox").value;
+                if (passValue == confirmPassValue) {
+                    document.getElementById("submitBox").disabled=false;
+                    document.getElementById("error").innerHTML="";
+                    console.log("equal");
+                }else{
+                    console.log("not equal");
+                    document.getElementById("error").innerHTML="*Not identical Password!";
+                    document.getElementById("submitBox").disabled=true;
+                }
+            }
+
+            document.getElementById("confirmPassBox").addEventListener("keyup",checkPass);
+
+        </script>
+    </body>
+</html>
