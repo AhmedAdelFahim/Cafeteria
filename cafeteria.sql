4f03058fb83caa259f50196f2eeb714a267f07e4
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-02-23 14:09:37 +0200

    update DB

--- cafeteria.sql
+++ cafeteria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 22, 2020 at 03:54 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.2
+-- Generation Time: Feb 23, 2020 at 01:01 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `cafeteria`
 --
+CREATE DATABASE IF NOT EXISTS `cafeteria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cafeteria`;
 
 -- --------------------------------------------------------
 
@@ -35,6 +37,13 @@ CREATE TABLE `categories` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'drinks', '2020-02-21 22:00:00', '2020-02-21 22:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -44,13 +53,24 @@ CREATE TABLE `categories` (
 CREATE TABLE `orders` (
   `id` int(255) NOT NULL,
   `status` varchar(255) COLLATE utf8_bin NOT NULL,
-  `product_id` int(255) NOT NULL,
-  `user_id` int(255) NOT NULL,
-  `total price` int(255) NOT NULL,
+  `product_id` int(255) DEFAULT NULL,
+  `user_id` int(255) DEFAULT NULL,
+  `total_price` int(255) NOT NULL,
   `notes` varchar(255) COLLATE utf8_bin NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `status`, `product_id`, `user_id`, `total_price`, `notes`, `created_at`, `updated_at`) VALUES
+(14, '0', NULL, NULL, 43, 'dffd', '2020-02-22 18:39:54', '2020-02-22 18:39:54'),
+(15, '0', NULL, NULL, 50, 'sddsd', '2020-02-22 18:40:51', '2020-02-22 18:40:51'),
+(16, '0', NULL, NULL, 91, '', '2020-02-22 19:03:38', '2020-02-22 19:03:38'),
+(17, '0', NULL, NULL, 24, 'saas', '2020-02-22 19:12:32', '2020-02-22 19:12:32'),
+(18, '0', NULL, NULL, 7, 'ds', '2020-02-22 19:12:40', '2020-02-22 19:12:40');
 
 -- --------------------------------------------------------
 
@@ -67,6 +87,14 @@ CREATE TABLE `products` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `picture`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 'Tea', 5, 'files/tea.png', 1, '2020-02-21 22:00:00', '2020-02-21 22:00:00'),
+(2, 'Coffee', 7, 'files/coffee.png', 1, '2020-02-21 22:00:00', '2020-02-21 22:00:00');
 
 -- --------------------------------------------------------
 
@@ -87,6 +115,13 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `roomNo`, `ext`, `picture`, `privilege`, `created_at`, `updated_at`) VALUES
+(1, 'Ahmed Adel', 'ahmed@gmail.com', '123', 2, 1234, 'files/a.png', '1', '2020-02-21 22:00:00', '2020-02-21 22:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -97,8 +132,24 @@ CREATE TABLE `users_orders` (
   `id` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
   `product_id` int(255) NOT NULL,
-  `order_id` int(255) NOT NULL
+  `order_id` int(255) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `users_orders`
+--
+
+INSERT INTO `users_orders` (`id`, `user_id`, `product_id`, `order_id`, `total_price`, `amount`) VALUES
+(7, 1, 2, 14, 28, 4),
+(8, 1, 1, 14, 15, 3),
+(9, 1, 1, 15, 50, 10),
+(10, 1, 2, 16, 56, 8),
+(11, 1, 1, 16, 35, 7),
+(12, 1, 2, 17, 14, 2),
+(13, 1, 1, 17, 10, 2),
+(14, 1, 2, 18, 7, 1);
 
 --
 -- Indexes for dumped tables
@@ -148,31 +199,31 @@ ALTER TABLE `users_orders`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users_orders`
 --
 ALTER TABLE `users_orders`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
