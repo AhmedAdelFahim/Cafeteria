4d3e03719d86e32b9d2b10ce6582eab82b2eb1d6
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-03-06 21:33:33 +0200

    Update cancelorder.php

--- cancelorder.php
+++ cancelorder.php
@@ -5,5 +5,5 @@ $id= $_GET['row'];
 $sql= "DELETE FROM orders where id=?";
 $stmt=$db->prepare($sql);
 $stmt->execute([$id]);
-header("Location: ordersUser.php");
+header("Location: ordersUser.php?from=". date("Y-m-d",time()-604800)."&to=".date("Y-m-d",time()+604800));
 ?>
