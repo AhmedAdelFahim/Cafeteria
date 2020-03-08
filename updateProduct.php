21600f97bc064f218037d960da1302a95b064f6f
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-03-08 04:51:17 +0200

    update user & update product bugs fixed

--- updateProduct.php
+++ updateProduct.php
@@ -1,9 +1,18 @@
 <?php
     require 'utils/DBConnection.php';
 
+    var_dump($_POST);
     $db = database_connection\DBConnection::getInstance();
 
-    $picturePath = "upload/".$_FILES['Product_Picture']['name'];
+    if(isset($_FILES['Product_Picture']['name'])&&!empty($_FILES['Product_Picture']['name'])){
+        $picturePath = "upload/".$_FILES['Product_Picture']['name'];
+        $file_name = $_FILES['Product_Picture']['name'];
+        $file_size = $_FILES['Product_Picture']['size'];
+        $file_tmp = $_FILES['Product_Picture']['tmp_name'];
+        $file_type = $_FILES['Product_Picture']['type'];
+
+        move_uploaded_file($file_tmp, 'upload/'.$file_name);
+    }
 
     $query = 'UPDATE products SET `name`=?, `price`=?, `picture`=?, category_id=? WHERE id=?';
 
--- updateUser.php
+++ updateUser.php
@@ -19,18 +19,21 @@
         $hash = password_hash($_POST['pass'], PASSWORD_BCRYPT, $options);
     }
 
-    $path = "upload/".$_FILES['profil']['name'];
-    
-    $file_name = $_FILES['profil']['name'];
-    $file_size = $_FILES['profil']['size'];
-    $file_tmp = $_FILES['profil']['tmp_name'];
-    $file_type = $_FILES['profil']['type'];
-    
-    move_uploaded_file($file_tmp, 'upload/'.$file_name);
+    if(isset($_FILES['profil']['name'])&&!empty($_FILES['profil']['name'])){
+
+        $path = "upload/".$_FILES['profil']['name'];
+
+        $file_name = $_FILES['profil']['name'];
+        $file_size = $_FILES['profil']['size'];
+        $file_tmp = $_FILES['profil']['tmp_name'];
+        $file_type = $_FILES['profil']['type'];
+
+        move_uploaded_file($file_tmp, 'upload/'.$file_name);
+    }
 
     $sql = 'UPDATE users SET name=?, email=?, password=?, roomNo=?, ext=?, picture=?, privilege=?
             where id=?';
     $stat = $conn->prepare($sql);
-    $stat->execute([$_POST['name'], $_POST['email'], $hash, $_POST['roomNo'], $_POST['ext'], 'upload/'.$_FILES['profil']['name'], 'user', $_GET['id']]);
+    $stat->execute([$_POST['name'], $_POST['email'], $hash, $_POST['roomNo'], $_POST['ext'], $path, 'user', $_GET['id']]);
 
     header('location:AllUsers.php?updated=1');
